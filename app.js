d0f1a76e8264abd2efd394baec44d59f88e50f3a
Noal Brown <noalbrown1@gmail.com>
2020-11-17 15:00:08 -0600

    changes

---
+++ app.js
@@ -0,0 +1,45 @@
+// Storage Controller
+
+// Item Controller
+const ItemCtrl = (function () {
+  // Item Constructor
+  const Item = function (id, name, calories) {
+    this.id = id;
+    this.name = name;
+    this.name = calories;
+  }
+  // Data Structure / State
+  const data = {
+    items: [
+      { id: 0, name: 'Steak Dinner', calories: 1200 },
+      { id: 1, name: 'Cookie', calories: 400 },
+      { id: 2, name: 'Eggs', calories: 300 }
+    ],
+    currentItem: null,
+    totalCalories: 0
+  }
+
+  return {
+    logData: function () {
+      return data;
+    }
+  }
+})();
+
+
+// UI Controller
+const UICtrl = (function () {
+
+})();
+
+// App Controller
+const App = (function (ItemCtrl, UICtrl) {
+  return {
+    init: function () {
+      console.log("init app")
+    }
+  }
+})(ItemCtrl, UICtrl);
+
+// Initialize app
+App.init()
