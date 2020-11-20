d8687b407a4c708f123cda98febf9338d63dfe45
Noal Brown <noalbrown1@gmail.com>
2020-11-20 15:01:00 -0600

    changes

--- app.js
+++ app.js
@@ -1,4 +1,35 @@
 // Storage Controller
+const StorageCtrl = (function () {
+  return {
+    storeItem: function (item) {
+      let items;
+
+      if (localStorage.getItem('items') === null) {
+        items = [];
+
+        items.push(item);
+
+        localStorage.setItem('items', JSON.stringify(items));
+      } else {
+        items = JSON.parse(localStorage.getItem('items'));
+
+        items.push(item);
+
+        localStorage.setItem('items', JSON.stringify(items));
+      }
+    },
+    getItemsFromStorage: function () {
+      let items;
+
+      if (localStorage.getItem('items') === null) {
+        items = []
+      } else {
+        items = JSON.parse(localStorage.getItem('items'))
+      }
+      return items;
+    }
+  }
+})()
 
 // Item Controller
 const ItemCtrl = (function () {
@@ -10,11 +41,12 @@ const ItemCtrl = (function () {
   }
   // Data Structure / State
   const data = {
-    items: [
-      // { id: 0, name: 'Steak Dinner', calories: 1200 },
-      // { id: 1, name: 'Cookie', calories: 400 },
-      // { id: 2, name: 'Eggs', calories: 300 }
-    ],
+    // items: [
+    //   // { id: 0, name: 'Steak Dinner', calories: 1200 },
+    //   // { id: 1, name: 'Cookie', calories: 400 },
+    //   // { id: 2, name: 'Eggs', calories: 300 }
+    // ],
+    items: StorageCtrl.getItemsFromStorage(),
     currentItem: null,
     totalCalories: 0
   }
@@ -217,7 +249,7 @@ const UICtrl = (function () {
 })();
 
 // App Controller
-const App = (function (ItemCtrl, UICtrl) {
+const App = (function (ItemCtrl, StorageCtrl, UICtrl) {
   const loadEventListeners = function () {
     const UISelectors = UICtrl.getSelectors();
 
@@ -252,6 +284,8 @@ const App = (function (ItemCtrl, UICtrl) {
       const totalCalories = ItemCtrl.getTotalCalories();
 
       UICtrl.showTotalCalories(totalCalories);
+
+      StorageController.storeItem(newItem);
 
       UICtrl.clearInput()
     }
@@ -340,7 +374,7 @@ const App = (function (ItemCtrl, UICtrl) {
       loadEventListeners();
     }
   }
-})(ItemCtrl, UICtrl);
+})(ItemCtrl, StorageCtrl, UICtrl);
 
 // Initialize app
 App.init()
